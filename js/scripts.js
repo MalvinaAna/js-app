e054066b76691ca9487b12c3edee61f757cc5563
MalvinaAna <47119139+MalvinaAna@users.noreply.github.com>
2023-10-04 00:02:43 +0300

    Updated scripts.js file
    Removed previous test code
    Created pokemonList array with 4 pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-let favoriteFood='Souvlaki';
-document.write(favoriteFood);
-
-alert('Hello World');
+let pokemonList= [
+    {
+    name: "Pikachu",
+    heigth: 0.4,
+    types: ['electric' , 'ground']
+    },
+    {
+    name:"Squirtle",
+    heigth: 0.5,
+    types: ['water']
+    },
+    {
+    name: "Charmander",
+    heigth:0.6,
+    types: ['fire']
+    },
+    {
+    name: "Jigglypuff",
+    heigth: 0.5,
+    types: ['normal' , 'fairy']
+    }
+]; 
