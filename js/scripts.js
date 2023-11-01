269ac8f520f65bcbc8646f1081ce2ce3e25eebd3
MalvinaAna <47119139+MalvinaAna@users.noreply.github.com>
2023-11-01 23:01:09 +0200

    Updated scripts.js
    Wrapped pokemonList array in an IIFE
    
    Added return object in the IIFE with the getAll and add public functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,43 @@
-let pokemonList= [
-    {
-    name: "Pikachu",
-    heigth: 0.4,
-    types: ['electric' , 'ground']
-    },
-    {
-    name:"Squirtle",
-    heigth: 0.5,
-    types: ['water']
-    },
-    {
-    name: "Charmander",
-    heigth:0.6,
-    types: ['fire']
-    },
-    {
-    name: "Jigglypuff",
-    heigth: 0.5,
-    types: ['normal' , 'fairy']
-    }
-]; 
+let pokemonRepository=(function(){
 
-pokemonList.forEach(function(pokemon){
+    let pokemonList= [
+        {
+            name: "Pikachu",
+            heigth: 0.4,
+            types: ['electric', 'ground']
+        },
+        {
+            name: "Squirtle",
+            heigth: 0.5,
+            types: ['water']
+        },
+        {
+            name: "Charmander",
+            heigth: 0.6,
+            types: ['fire']
+        },
+        {
+            name: "Jigglypuff",
+            heigth: 0.5,
+            types: ['normal', 'fairy']
+        }
+    ]
+
+    function getAll(){
+        return pokemonList;
+    }
+
+    function add(pokemon){
+        return pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+})()
+
+pokemonRepository.getAll().forEach(function(pokemon){
     if (pokemon.heigth > 0.5) {
         document.write('<p>' + pokemon.name + ' (heigth:' + pokemon.heigth + ') - Wow, that\’s big!' + '</p>');
     } else {  
